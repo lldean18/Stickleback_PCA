6f6951384e1f9c3138fcc518226067e6a75b41a6
Laura Dean <lldean18@gmail.com>
2025-01-17 21:55:45 +0000

    adding a script to filter vcf for quality

---
+++ 16_filter_VCF_QUAL.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Laura Dean
+# 17/1/25
+# for running on the UoN HPC Ada
+
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=50g
+#SBATCH --time=24:00:00
+#SBATCH --job-name=stickle_flt_vcf
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+  
+############################
+   # PREPARE ENVIRONMENT #
+############################
+
+# set variables
+wkdir=/gpfs01/home/mbzlld/data/stickleback
+species=stickleback
+VCF=$wkdir/vcfs/${species}_SNPs_nomissing.vcf.gz
+
+# load modules
+module load vcftools-uoneasy/0.1.16-GCC-12.3.0
+module load bcftools-uoneasy/1.18-GCC-13.2.0
+
+###################
+# Filter vcf file #
+###################
+
+# remove missing data
+vcftools \
+--gzvcf $VCF \
+--minQ 30 \
+--recode \
+--recode-INFO-all \
+--stdout |
+bcftools view -Oz -o ${VCF%.*.*}_Qual30.vcf.gz
+bcftools index ${VCF%.*.*}_Qual30.vcf.gz
+
+
+# unload modules
+module unload vcftools-uoneasy/0.1.16-GCC-12.3.0
+module unload bcftools-uoneasy/1.18-GCC-13.2.0
+
