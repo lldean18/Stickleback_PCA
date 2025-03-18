1234b728b844d79a14b87dc7bc154dfd480becba
Laura Dean <lldean18@gmail.com>
2025-03-18 05:50:43 +0000

    pasted the wrong file here when I added them to github so updating this to be the right script

--- 04_cat_fastq_files.bash
+++ 04_cat_fastq_files.bash
@@ -1,34 +1,37 @@
 #!/bin/bash
 # Laura Dean
-# 18/11/24
-# for running on UoN HPC Ada
+# 20/11/24
 
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=60g
-#SBATCH --time=168:00:00
-#SBATCH --job-name=Trimmed_backup
+#SBATCH --mem=40g
+#SBATCH --time=15:00:00
+#SBATCH --job-name=catFqs
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
-# Had to re-do the rclone config because it had been too long since I used it
-# instructions for this are in the Bioinftech dir on my onedrive
+################################################################################################################
+# BIG DATA with both final repeats: Dec 2024 concatenate runs for each individual into new properly named .fastq.gz files
+################################################################################################################
 
-# load the Rclone module
-module load rclone-uon/1.65.2
 
-# drive options for rclone
-#OrgOne MacCollLab1 MacCollLab2 Laura
+# Tried running on login node but would have taken hrs so submitted as a slurm job 
 
-# Copy the fastq files from sharepoint to Ada
-rclone --transfers 1 --checkers 1 --bwlimit 100M --checksum copy /gpfs01/home/mbzlld/data/stickleback/Trimmed MacCollLab2:HPC_data_backup/bigdata/Trimmed
- 
+cd ~/data/stickleback/Trimmed # move to directory with sequencing read folders
 
-# and check that the two folders are identical
-rclone check --one-way /gpfs01/home/mbzlld/data/stickleback/Trimmed MacCollLab2:HPC_data_backup/bigdata/Trimmed
+mkdir -p ~/data/stickleback/trimmed_fqs # make directory to put new files in (if it doesn't already exist)
 
-# unload the rclone module
-module unload rclone-uon/1.65.2
+for dir in */
+do
+
+indname=${dir##*-} # extract individual names
+indname=${indname%/*} # remove trailing slash from indnames
+
+cat $(find ./$dir -name "*_R1*" | sort) > ~/data/stickleback/trimmed_fqs/${indname}_R1.fastq.gz # find all R1 files and cat their contents to a new file write the contents of these files to another file
+cat $(find ./$dir -name "*_R2*" | sort) > ~/data/stickleback/trimmed_fqs/${indname}_R2.fastq.gz # find all R2 files and cat their contents to a new file write the contents of these files to another file
+cat $(find ./$dir -name "*_R0*" | sort) > ~/data/stickleback/trimmed_fqs/${indname}_R0.fastq.gz # find all R0 files and cat their contents to a new file write the contents of these files to another file
+
+done
+
 
